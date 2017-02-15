a2c8e98f55f850ef0a0e5f0273ae8094795bc676
Pierluigi Di Cerbo <pierluigidicerbo@dhcp95-v34.pdl.cineca.it>
2017-02-15 15:53:52 +0100

    adapting code for exercise

--- day3/IntraNodeOptimization/ex1/ex1.2/datatype_d.c
+++ day3/IntraNodeOptimization/ex1/ex1.2/datatype_d.c
@@ -3,18 +3,20 @@
 #include <omp.h>
 #include <math.h>
 
+typedef signed int MyType;
+
 int main()
 {
 
-double time,time1;
+MyType time,time1;
 int t,i,j,k,totalsize,size = 500,repetition=40;
 
 
 totalsize = size * size * size;
 
-double * a = (double *) _mm_malloc(totalsize * sizeof(double),64);
-double * b = (double *) _mm_malloc(totalsize * sizeof(double),64);
-double * c = (double *) _mm_malloc(totalsize * sizeof(double),64);
+MyType * a = (MyType *) _mm_malloc(totalsize * sizeof(MyType),64);
+MyType * b = (MyType *) _mm_malloc(totalsize * sizeof(MyType),64);
+MyType * c = (MyType *) _mm_malloc(totalsize * sizeof(MyType),64);
 
 for(int i=0;i<totalsize;i++)
 {
@@ -32,7 +34,7 @@ for(int t=0;t<repetition;t++)
 
 time = omp_get_wtime() - time;
 
-printf("TIME double %e \n",time/repetition);
+printf("TIME double (with sizeof(MyType) = %d) is %e \n",sizeof(MyType), time/repetition);
 
 _mm_free(a);
 _mm_free(b);
