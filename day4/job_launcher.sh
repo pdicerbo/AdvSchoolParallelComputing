1b6e66a246fbccfdd1f0f3f1797aab140772b6e8
Nicola Spallanzani <n.spallanzani@cineca.it>
2017-02-15 19:11:34 +0100

    Update job_launcher.sh

--- day4/job_launcher.sh
+++ day4/job_launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N python_HPC
-#PBS -A cin_staff
+#PBS -A train_scA2017
 #PBS -q R457635
 #PBS -l select=1:ncpus=1:mpiprocs=1:mem=1Gb
 ###PBS -l select=2:ncpus=36:mpiprocs=36:mem=118Gb
@@ -13,7 +13,7 @@ module load python/2.7.12
 module load gnu/6.1.0
 #module load openmpi/1-10.3--gnu--6.1.0
 
-#source /marconi_scratch/userinternal/nspalla1/TRAINING/python4HPC/env/bin/activate
+#source env/bin/activate
 
 rm -f inputs/*.out
 
