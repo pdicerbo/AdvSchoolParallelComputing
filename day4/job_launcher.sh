53f413e76385c6610083fb700a5b254a0ae71912
Nicola Spallanzani <n.spallanzani@cineca.it>
2017-02-15 18:23:05 +0100

    input esercizi python4HPC

---
+++ day4/job_launcher.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#PBS -N python_HPC
+#PBS -A cin_staff
+#PBS -q R457635
+#PBS -l select=1:ncpus=1:mpiprocs=1:mem=1Gb
+###PBS -l select=2:ncpus=36:mpiprocs=36:mem=118Gb
+#PBS -l walltime=00:30:00
+
+cd $PBS_O_WORKDIR
+
+module purge
+module load python/2.7.12
+module load gnu/6.1.0
+#module load openmpi/1-10.3--gnu--6.1.0
+
+#source /marconi_scratch/userinternal/nspalla1/TRAINING/python4HPC/env/bin/activate
+
+rm -f inputs/*.out
+
+./single_queue.py serial_program.x inputs
+#mpirun ./multiple_queues.py serial_program.x inputs
+#mpirun ./master_slave.py serial_program.x inputs
+#./fault_tollerance.py mpi_program.x input.txt > out.txt 2> err.txt
+
+#deactivate
